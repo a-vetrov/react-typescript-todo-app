c1d9d1acfddafb96d910fde3b983144a3bf8c5b5
Ветров Андрей Алексеевич <Vetrov.A.Al@sberbank.ru>
2021-07-29 12:30:41 +0300

    Project started

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './App.module.css'
+
+function App() {
+  return (
+    <div className={style.container}>
+      ToDo app
+    </div>
+  );
+}
+
+export default App;
